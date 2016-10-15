f77e3580714233c111a15135aeb540ef075fbde6
Mengxiao Lin <linmx0130@163.com>
2016-10-15 09:13:15 +0800

    start project

---
+++ spec/intelli-helpon-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IntelliHelponView from '../lib/intelli-helpon-view';
+
+describe('IntelliHelponView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
