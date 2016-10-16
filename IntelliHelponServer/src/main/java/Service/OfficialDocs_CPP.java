70026807ac2710957eb2ba5b3753ce347a16bbe0
unknown <>
2016-10-16 09:39:30 +0800

    add OfficialDocs support for CPP

--- IntelliHelponServer/src/main/java/Service/OfficialDocs_CPP.java
+++ IntelliHelponServer/src/main/java/Service/OfficialDocs_CPP.java
@@ -1,6 +1,14 @@
 package Service;
 
+import Model.DocType;
+import Model.Item;
 import Model.ResultEntity;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by Mengxiao Lin on 2016-10-15.
@@ -8,28 +16,35 @@ import Model.ResultEntity;
  */
 public class OfficialDocs_CPP implements IService {
     private static final String CPP_DOCS =
-        "http://en.cppreference.com/mwiki/index.php?search=";
+            "http://en.cppreference.com/mwiki/index.php?search=";
+
     @Override
-    public ResultEntity getResult(String keyword) {
+    public ResultEntity getResult(String keyword) throws IOException {
         ResultEntity en = new ResultEntity();
-        String html = NetWorking.GET(CPP_DOCS + keyword +String_DOCS2);
+        String html;
+        html = NetWorking.GET(CPP_DOCS + keyword);
+
         Document doc = Jsoup.parse(html);
-        Elements resultObject = doc.getElementsByClass("mw-search-results").first();
-        if (resultObject == null){
+        Element resultObject = doc.getElementsByClass("mw-search-results").first();
+        if (resultObject == null) {
             return en;
         }
         Elements search_results = resultObject.children();
         for (Element ele : search_results) {
             Element result_link = ele.getElementsByTag("a").first();
-            String href = link.attr("href");
-            String title = link.text();
+            String href = result_link.attr("href");
+            String title = result_link.text();
             String summary_text = title;
             String summary_html = title;
-            System.out.println(href);
-            System.out.println(title);
-            System.out.println(summary_text);
-            System.out.println(summary_html);
-            System.out.println("---------------------------");
+
+
+//            System.out.println(href);
+//            System.out.println(title);
+//            System.out.println(summary_text);
+//            System.out.println(summary_html);
+//            System.out.println("---------------------------");
+
+
             Item i = new Item(DocType.OfficialDocs, title, summary_text, href);
             en.addItem(i);
         }
